f198e98b1d33201accceb60e0ebf4b9a0006045b
ChloeJu <lj1015@nyu.edu>
2019-11-05 16:15:06 -0500

    Start working on item: pick up

---
+++ Assets/Scripts/PickupableItem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//the tag of the thing picking up the object has to be "hand"
+//also in the inspector u have to link "parentPlayer" to the hand 
+///
+
+public class PickupableItem : MonoBehaviour
+{   // Start is called before the first frame update
+    public bool touchedOrNot = false;
+
+    public GameObject parentPlayer;
+    //public GameObject childObject;
+    public bool beingPickedUp = false;
+
+    // public beingHeld=false;
+
+    Rigidbody myRB;
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (Input.GetKey(KeyCode.Space) && other.CompareTag("hand"))
+        {
+            Debug.Log("touched");
+            if (true)
+            {
+                myRB = this.GetComponent<Rigidbody>();
+                // touchedOrNot = true;
+                // Destroy(this.gameObject);
+                this.transform.parent = parentPlayer.transform;
+                //this.transform.position = parentPlayer.transform.position;
+                // myRB.isKinematic = false;
+                beingPickedUp = true;
+                myRB.isKinematic = true;
+            }
+
+
+        }
+
+
+    }
+    void Start()
+    {
+        // this.transform.parent = parentPlayer.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {// this.transform.parent = parentPlayer.transform;
+        if (beingPickedUp == true && Input.GetKey(KeyCode.B))
+        {
+            myRB = this.GetComponent<Rigidbody>();
+            myRB.isKinematic = false;
+            this.transform.parent = null;
+            beingPickedUp = false;
+            // 
+
+
+
+        }
+
+    }
+}
