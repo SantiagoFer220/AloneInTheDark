2648e467507f489c8d4b5ed8a50d7b698549879b
Gabrielle Maia Dias <gmd371@nyu.edu>
2019-11-05 16:34:53 -0500

    Code and cameras placed.

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public Camera cameraone;
+    public Camera cameratwo;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       /* RaycastHit hit;
+        Ray ray = cameraone.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("im being hit!");
+        }
+*/
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
