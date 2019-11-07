d4ba5863913a7ce40bea645fd112589c52285fa7
Gabrielle Maia Dias <gmd371@nyu.edu>
2019-11-07 17:16:58 -0500

    Monster controller raycasting updated

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MonsterController : MonoBehaviour
 {
+    public float movespeed;
+
+    private float maxdist = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class MonsterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray myRay = new Ray(this.transform.position, this.transform.forward);
+        Debug.DrawRay(this.transform.position, this.transform.forward, Color.red, maxdist);
+
+        if (Physics.Raycast(myRay, maxdist))
+        {
+            Debug.Log("hit!");
+        }
+        else
+        {
+            this.transform.Translate(0, 0, movespeed * Time.deltaTime);
+        }
     }
 }
