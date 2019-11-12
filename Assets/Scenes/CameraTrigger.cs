59bba5c041bca8dc1d6979a9371af0ec89c90822
SantiagoFer <49788541+SantiagoFer@users.noreply.github.com>
2019-11-12 15:39:40 -0500

    Camera system is functional just needs to be implemented in the scene

---
+++ Assets/Scenes/CameraTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    public Shot targetShot;
+
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Player"))
+        {
+            targetShot.CutToShot();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Shot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This script deals with the different camera attributes we can change for every camera
+public class Shot: MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CutToShot()
+    {
+        Camera.main.transform.localPosition = transform.position;
+        Camera.main.transform.localRotation = transform.rotation;
+    }
+}
