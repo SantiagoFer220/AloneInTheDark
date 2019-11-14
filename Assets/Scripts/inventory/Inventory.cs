44b73acc85d665ab8435dd2a74ff980a310c4661
ChloeJu <lj1015@nyu.edu>
2019-11-14 15:10:45 -0500

    Inventory finally working

--- Assets/Scripts/inventory/Inventory.cs
+++ Assets/Scripts/inventory/Inventory.cs
@@ -5,12 +5,30 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
+    public static int numOfCurrentSlot = 0;
     public const int numItemSlots = 4;
     public Image[] itemImages = new Image[numItemSlots];
     public Item[] items = new Item[numItemSlots];
     public bool added = false;
     public void AddItem(Item itemToAdd) {
-        
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (items[i] == null)
+            {
+                items[i] = itemToAdd;
+                itemImages[i].sprite = itemToAdd.sprite;
+                itemImages[i].enabled = true;
+                return;
+            }
+        }
+        /*
+        items[numOfCurrentSlot] = itemToAdd;
+        itemImages[numOfCurrentSlot].sprite = itemToAdd.sprite;
+        itemImages[numOfCurrentSlot].enabled = true;
+        numOfCurrentSlot++;
+        */
+
+        /*
         for (int i = 0; i < items.Length; i++)
         {
             if (added == false)
@@ -21,6 +39,8 @@ public class Inventory : MonoBehaviour
                     itemImages[i].sprite = itemToAdd.sprite;
                     itemImages[i].enabled = true;
                     added = true;
+                    itemToAdd = null;
+                    itemToAdd.sprite = null;
                     return;
 
 
@@ -28,11 +48,11 @@ public class Inventory : MonoBehaviour
             }
             added = false;
         }
-        
-
+     */
     }
 
     public void RemoveItem(Item itemToRemove) {
+
         for (int i = 0; i < items.Length; i++) {
             if (items[i] == itemToRemove) {
                 items[i] = null;
--- Assets/Scripts/inventory/Item.cs
+++ Assets/Scripts/inventory/Item.cs
@@ -1,10 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class Item : ScriptableObject
 {
     public Sprite sprite;
+
+    public GameObject inventoryObject;
+    public Inventory myInven;
+    public bool touchedOrNot = false;
+    public Text text;
+    public GameObject parentPlayer;
+
+    public bool beingPickedUp = false;
+
+    // public beingHeld=false;
+    public GameObject rigidbodyObject;
+
+    Rigidbody myRB;
+
+
+    //the following script pick up items
+    //the input is to be modified. might need to access menu first
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (Input.GetKey(KeyCode.Space) && other.CompareTag("hand"))
+        {
+            Debug.Log("touched");
+            if (true)
+            {
+                myRB = rigidbodyObject.GetComponent<Rigidbody>();
+                // touchedOrNot = true;
+                // Destroy(this.gameObject);
+                rigidbodyObject.transform.parent = parentPlayer.transform;
+                //this.transform.position = parentPlayer.transform.position;
+                // myRB.isKinematic = false;
+                beingPickedUp = true;
+                myRB.isKinematic = true;
+
+                //myInven.AddItem()
+            }
+
+
+        }
+
+
+    }
+    void Start()
+    {
+        // this.transform.parent = parentPlayer.transform;
+        myInven = inventoryObject.GetComponent<Inventory>();
+    }
+
+
+    void Update()
+    {// this.transform.parent = parentPlayer.transform;
+
+        //the following thing DROP the item
+        if (beingPickedUp == true && Input.GetKey(KeyCode.B))
+        {
+            myRB = rigidbodyObject.GetComponent<Rigidbody>();
+            myRB.isKinematic = false;
+            rigidbodyObject.transform.parent = null;
+            beingPickedUp = false;
+            // 
+
+
+
+        }
+
+    }
 
 }
