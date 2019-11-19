2e32f5c53e6344776ca281f979cf135db019d362
SantiagoFer <49788541+SantiagoFer@users.noreply.github.com>
2019-11-19 16:30:31 -0500

    Redone map and more camera control options

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//This script is attached to the different Camera position objects. It sets the cameras future location and framing. It sets up the "Shots"
+//This script is attached to the different Camera position objects.\
+//It sets the cameras future location and framing. It sets up the "Shots"
 public class Shot: MonoBehaviour
 {
+
+    public Vector3 focalPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +19,27 @@ public class Shot: MonoBehaviour
     {
         
     }
-
+    //this transforms the camera to the right position and focal point
     public void CutToShot()
     {
+        transform.LookAt(focalPoint);
         Camera.main.transform.localPosition = transform.localPosition;
         Camera.main.transform.localRotation = transform.localRotation;
+    }
+
+    //switches cameras while in edit mode for easier setup
+    void OnDrawGizmosSelected()
+    {
+        if (!Application.isPlaying)
+        {
+            CutToShot();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, focalPoint);
+
     }
 }
