d8b5be9e1e4540bac9b097c6cff1b10a26c23725
ChloeJu <lj1015@nyu.edu>
2019-11-26 14:33:09 -0500

    add key object

--- Assets/Scripts/inventory/Item.cs
+++ Assets/Scripts/inventory/Item.cs
@@ -11,7 +11,73 @@ public class Item : MonoBehaviour
     public Text text;
 
     public Sprite sprite;
+    public GameObject inventoryObject;
+    public Inventory myInven;
+    public bool touchedOrNot = false;
+    //public Text text;
+    public GameObject parentPlayer;
 
- 
+    public bool beingPickedUp = false;
+
+    // public beingHeld=false;
+
+    //Rigidbody myRB;
+
+
+    //the following script pick up items
+    //the input is to be modified. might need to access menu first
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (Input.GetKeyDown(KeyCode.P) && other.CompareTag("Player"))
+        {
+            Debug.Log("touched");
+            if (true)
+            {
+                //   myRB = this.GetComponent<Rigidbody>();
+                // touchedOrNot = true;
+                // Destroy(this.gameObject);
+                //  this.transform.parent = parentPlayer.transform;
+                //this.transform.position = parentPlayer.transform.position;
+                // myRB.isKinematic = false;
+                beingPickedUp = true;
+                myInven.AddItem(this);
+                this.transform.position = new Vector3(999,999,999);
+                //  myRB.isKinematic = true;
+
+                //myInven.AddItem()
+            }
+
+
+        }
+
+
+    }
+    void Start()
+    {
+        // this.transform.parent = parentPlayer.transform;
+        myInven = inventoryObject.GetComponent<Inventory>();
+    }
+
+
+    void Update()
+    {// this.transform.parent = parentPlayer.transform;
+
+        //the following thing DROP the item
+        if (beingPickedUp == true && Input.GetKey(KeyCode.B))
+        {
+            //   myRB = this.GetComponent<Rigidbody>();
+            //  myRB.isKinematic = false;
+            //  this.transform.parent = null;
+            beingPickedUp = false;
+            // 
+
+
+
+        }
+
+    }
+
+
 
 }
