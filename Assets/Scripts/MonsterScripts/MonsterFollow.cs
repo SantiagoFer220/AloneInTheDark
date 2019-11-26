8d4274f9da0845caacfc7d1dfc7ad9bbceb138de
Gabrielle Maia Dias <gmd371@nyu.edu>
2019-11-26 17:16:26 -0500

    Monster stuff

--- Assets/Scripts/MonsterScripts/MonsterFollow.cs
+++ Assets/Scripts/MonsterScripts/MonsterFollow.cs
@@ -8,7 +8,9 @@ public class MonsterFollow : MonoBehaviour
     public GameObject player;
     public float dist;
     public float movespeed = 2.0f;
-    // Start is called before the first frame update
+    public float maxdistfromplayer;
+
+// Start is called before the first frame update
     void Start()
     {
         //player location, can be moved
@@ -21,7 +23,7 @@ public class MonsterFollow : MonoBehaviour
         // distance between the monster and the player
         dist = Vector3.Distance(player.transform.position, transform.position);
 
-        if (dist < 12)
+        if (dist < maxdistfromplayer)
         {
             // see the player, then use the given speed and position to move towards the player position on the maps
             transform.LookAt(player.transform);
