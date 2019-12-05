e082dfb2e0793939523d022e13efcefcdfe14d77
Gabrielle Maia Dias <gmd371@nyu.edu>
2019-12-05 13:10:57 -0500

    Fixed Monster chasing and updated health system. Collision not working tho.

--- Assets/Scripts/MonsterScripts/MonsterChase.cs
+++ Assets/Scripts/MonsterScripts/MonsterChase.cs
@@ -1,29 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
- 
-public class MonsterChase : MonoBehaviour {
-    NavMeshAgent agent;
-    int currentWaypoint = 0;﻿
+
+public class MonsterChase : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    int _currentWaypoint = 0;
     [SerializeField] float decisionDelay = 2f;
     [SerializeField] Transform objectToChase;
-    [SerializeField] Transform[] waypoints;﻿
-    
+    [SerializeField] Transform[] waypoints;
+
     [SerializeField] EnemyStates currentState;
-    enum EnemyStates {
+
+    enum EnemyStates
+    {
         Patrolling,
         Chasing
-        
+
     }
-    
-    void Start () {
-        agent = GetComponent<NavMeshAgent>();
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
         InvokeRepeating("SetDestination", 1.5f, decisionDelay);
-        if(currentState == EnemyStates.Patrolling) agent.SetDestination(waypoints[currentWaypoint].position);﻿
+        if (currentState == EnemyStates.Patrolling) _agent.SetDestination(waypoints[_currentWaypoint].position);
     }
 
     void Update()
-    {if(Vector3.Distance(transform.position, objectToChase.position) >10f)
+    {
+        if (Vector3.Distance(transform.position, objectToChase.position) > 10f)
         {
             currentState = EnemyStates.Patrolling;
         }
@@ -31,31 +36,36 @@ public class MonsterChase : MonoBehaviour {
         {
             currentState = EnemyStates.Chasing;
         }
-        
-        if(currentState == EnemyStates.Patrolling)
+
+        if (currentState == EnemyStates.Patrolling)
         {
-            if(Vector3.Distance(transform.position, waypoints[currentWaypoint].position) < 0.6f)
+            if (Vector3.Distance(transform.position, waypoints[_currentWaypoint].position) < 0.6f)
             {
-                currentWaypoint++;
-                if (currentWaypoint == waypoints.Length)
+                _currentWaypoint++;
+                if (_currentWaypoint == waypoints.Length)
                 {
-                    currentWaypoint = 0;
+                    _currentWaypoint = 0;
                 }
             }
-            agent.SetDestination(waypoints[currentWaypoint].position);
+
+            _agent.SetDestination(waypoints[_currentWaypoint].position);
         }
     }
-    void SetDestination() {
-        if(currentState == EnemyStates.Chasing) agent.SetDestination(objectToChase.position);
+
+    void SetDestination()
+    {
+        if (currentState == EnemyStates.Chasing) _agent.SetDestination(objectToChase.position);
     }
-    
-    //what happens when the mnster catches yhe pleyer
+
+//what happens when the mnster catches yhe pleyer
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.name == "Player")
         {
-            //Heres where health/death will go
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+            //Heres where health/death will go for monster
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
-}﻿
+}
+      
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-public float moveSpeed;
-public float turnSpeed;
-public KeyCode Forward; 
-public KeyCode Backward;
-public KeyCode RotateRight;
-public KeyCode RotateLeft;
+    public float moveSpeed;
+    public float turnSpeed;
+    public KeyCode Forward;
+    public KeyCode Backward;
+    public KeyCode RotateRight;
+    public KeyCode RotateLeft;
+
+
+    ////////PlayerDamage
+    public int hp;
+    public bool isdead;
+    public bool letplay;
+
 
     /////////////INVENTORY CRAP
     // //////////INVENTORY CRAP
     public GameObject inventoryObject;
-  public Inventory myInventory;
-   public Item myItem;
+    public Inventory myInventory;
+    public Item myItem;
     public Item anohterItem;
     public bool InventoryIsActive;
-    public bool isSelectingActions=false;
+    public bool isSelectingActions = false;
 
 
 
@@ -34,97 +42,139 @@ public KeyCode RotateLeft;
     // this sets the movement to different keys set in the inspector. 
     void Update()
     {
-        if ( !inventoryObject.activeSelf)//if inventory is NOT enabled
+        if (!inventoryObject.activeSelf) //if inventory is NOT enabled
         {
-            if (Input.GetKey(Forward))//&&!Input.GetKey(RotateLeft) && !Input.GetKey(RotateRight)) 
+            if (Input.GetKey(Forward)) //&&!Input.GetKey(RotateLeft) && !Input.GetKey(RotateRight)) 
             {
                 this.transform.Translate(0f, 0f, moveSpeed * Time.deltaTime);
             }
 
-            if (Input.GetKey(Backward) )//&& !Input.GetKey(RotateLeft) && !Input.GetKey(RotateRight)) 
-        {
+            if (Input.GetKey(Backward)) //&& !Input.GetKey(RotateLeft) && !Input.GetKey(RotateRight)) 
+            {
                 this.transform.Translate(0f, 0f, -moveSpeed * Time.deltaTime);
             }
 
-            if (Input.GetKey(RotateLeft))//&& !Input.GetKey(Forward) && !Input.GetKey(Backward))
-        {
+            if (Input.GetKey(RotateLeft)) //&& !Input.GetKey(Forward) && !Input.GetKey(Backward))
+            {
                 this.transform.Rotate(0f, -turnSpeed, 0f);
             }
 
-            if (Input.GetKey(RotateRight))// && !Input.GetKey(Forward) && !Input.GetKey(Backward))
-    {
+            if (Input.GetKey(RotateRight)) // && !Input.GetKey(Forward) && !Input.GetKey(Backward))
+            {
                 this.transform.Rotate(0f, turnSpeed, 0f);
             }
         }
 
-
-        //////////////////INVENTORY CRAP
-        //////////////////INVENTORY CRAP
-        //////////////////INVENTORY CRAP
-        //////////////////INVENTORY CRAP
-        //////////////////INVENTORY CRAP
-        if (inventoryObject.activeSelf)
+        ////////Player Damage
+        int GetHP()
         {
-
-
-            //////////////////INVENTORY CRAP
-            ///
-            //Add object
-            if (Input.GetKeyDown(KeyCode.B))
-            {
-                Debug.Log("space pressed to initiate interface");
-                myInventory.AddItem(myItem);
-            }
-            if (Input.GetKeyDown(KeyCode.V))
-            {
-                Debug.Log("space pressed to initiate interface");
-                myInventory.AddItem(anohterItem);
-            }
-            //delete object
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                Debug.Log("222space pressed to initiate interface");
-                myInventory.RemoveItem(myItem);
-            }
-            if (Input.GetKeyDown(KeyCode.X))
-            {
-                Debug.Log("222space pressed to initiate interface");
-                myInventory.RemoveItem(anohterItem);
-            }
-
+            return hp;
         }
 
-        //Close and Open Inventory window 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-         //   if(inventoryObject.ac)
-            inventoryObject.SetActive(!inventoryObject.activeSelf);
-
-        }
-
-        //selecting items
-        if (inventoryObject.activeSelf && isSelectingActions==false) {
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                myInventory.current++;
-            }
-
-        }
-
-        //selecting actions on a certain item
-        if (inventoryObject.activeSelf && isSelectingActions == true)
+        void HurtPlayer(int amount)
         {
-            if (Input.GetKeyDown(KeyCode.S))
+            hp -= amount;
+
+            if (hp <= 0)
             {
-            
-                myInventory.currentAction++;
+                Debug.Log("Im dead!");
+                isdead = true;
             }
 
+            if (isdead)
+            {
+                //Destroy(gameObject);
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                letplay = !letplay;
+            }
         }
 
-        if (inventoryObject.activeSelf && Input.GetKeyDown(KeyCode.Return)) {
-            isSelectingActions = !isSelectingActions;
 
+        void OnCollisionEnter(Collider collision)
+            {
+                if (collision.gameObject.tag == "monster")
+                {
+                    Debug.Log("ouch");
+                    HealthSystem2 monster = collision.GetComponent<HealthSystem2>();
+
+                    HurtPlayer(monster.strength);
+                }
+            }
+        
+
+//////////////////INVENTORY CRAP
+//////////////////INVENTORY CRAP
+//////////////////INVENTORY CRAP
+//////////////////INVENTORY CRAP
+//////////////////INVENTORY CRAP
+            if (inventoryObject.activeSelf)
+            {
+
+
+                //////////////////INVENTORY CRAP
+                ///
+                //Add object
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    Debug.Log("space pressed to initiate interface");
+                    myInventory.AddItem(myItem);
+                }
+
+                if (Input.GetKeyDown(KeyCode.V))
+                {
+                    Debug.Log("space pressed to initiate interface");
+                    myInventory.AddItem(anohterItem);
+                }
+
+                //delete object
+                if (Input.GetKeyDown(KeyCode.C))
+                {
+                    Debug.Log("222space pressed to initiate interface");
+                    myInventory.RemoveItem(myItem);
+                }
+
+                if (Input.GetKeyDown(KeyCode.X))
+                {
+                    Debug.Log("222space pressed to initiate interface");
+                    myInventory.RemoveItem(anohterItem);
+                }
+
+            }
+
+//Close and Open Inventory window 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //   if(inventoryObject.ac)
+                inventoryObject.SetActive(!inventoryObject.activeSelf);
+
+            }
+
+//selecting items
+            if (inventoryObject.activeSelf && isSelectingActions == false)
+            {
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    myInventory.current++;
+                }
+
+            }
+
+//selecting actions on a certain item
+            if (inventoryObject.activeSelf && isSelectingActions == true)
+            {
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+
+                    myInventory.currentAction++;
+                }
+
+            }
+
+            if (inventoryObject.activeSelf && Input.GetKeyDown(KeyCode.Return))
+            {
+                isSelectingActions = !isSelectingActions;
+
+            }
         }
     }
-}
 
