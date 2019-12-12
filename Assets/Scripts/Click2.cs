db94f1468ee07d8f0f9866dc7fd1b418fc8abd02
Gabrielle Maia Dias <gmd371@nyu.edu>
2019-12-12 16:50:22 -0500

    Music, start and end scenes and more.
    Build done.

---
+++ Assets/Scripts/Click2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Click2 : MonoBehaviour
+{
+
+    public int SceneNum = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneNum);
+    }
+}
---
+++ Assets/Scripts/WinCon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCon : MonoBehaviour
+{
+   
+    public int SceneNum = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider collision)
+
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+            ChangeScene();
+        }
+    }
+
+
+
+
+
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneNum);
+    }
+}
